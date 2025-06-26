d3152286fbe26ac91e8ca14d9b66c50dec88f03c
almaz <almazgimaev@gmail.com>
2025-06-26 16:00:22 +0200

    Update docker image version to 1.0.2 in config and build scripts

--- docker/build_push.sh
+++ docker/build_push.sh
@@ -1,4 +1,4 @@
 cp ../dev_requirements.txt . && \
-docker build --no-cache -t supervisely/projections-service:1.0.1 . && \
+docker build --no-cache -t supervisely/projections-service:1.0.2 . && \
 rm dev_requirements.txt && \
-docker push supervisely/projections-service:1.0.1
+docker push supervisely/projections-service:1.0.2
